ba44f56597fda62cb003811a54a3d6a41365919e
Henry6803 <mesopro1968@gmail.com>
2025-05-17 02:29:13 +0800

    Update db.py

--- python/utils/db.py
+++ python/utils/db.py
@@ -3,12 +3,11 @@ import os
 
 def get_db_connection():
     conn = mysql.connector.connect(
-        host=os.environ.get("MYSQL_HOST", "sarawaktourismdb.mysql.database.azure.com"),
-        user=os.environ.get("MYSQL_USER", "Henry@sarawaktourismdb"),
-        password=os.environ.get("MYSQL_PASSWORD", "Mesopro123"),
-        database=os.environ.get("MYSQL_DATABASE", "sarawaktourismdb"),
+        host=os.environ.get("MYSQL_HOST", "stdb3.mysql.database.azure.com"),
+        user=os.environ.get("MYSQL_USER", "Henry@stdb3"),
+        password=os.environ.get("MYSQL_PASSWORD", "Hello123"),
+        database=os.environ.get("MYSQL_DATABASE", "sarwak_tourism"),
         port=int(os.environ.get("MYSQL_PORT", "3306")),
-        ssl_ca=os.environ.get("MYSQL_SSL_CA", "DigiCertGlobalRootCA.crt.pem"),
         ssl_disabled=False
     )
     return conn
