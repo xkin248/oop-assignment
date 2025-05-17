6bdfdb3119d949e547996fa20785dbb68d30510b
Henry6803 <mesopro1968@gmail.com>
2025-05-17 07:51:16 +0000

    .

--- python/utils/db.py
+++ python/utils/db.py
@@ -1,36 +1,31 @@
 import mysql.connector
-import os
+from mysql.connector import Error
+
 config = {
-  "user": "Henry@stdb3",
-  "password": "Hello123",
-  "host": "stdb3.mysql.database.azure.com",
-  "port": 3306,
-  "database": "sarawk_tourism",
-  "ssl_ca": "{oop/-assignment/python/static/DigiCertGlobalRootCA.crt.pem}",
-  "ssl_disabled": False
+    "user": "Henry@stdb3",
+    "password": "Hello123",
+    "host": "stdb3.mysql.database.azure.com",
+    "port": 3306,
+    "database": "sarawk_tourism",
+    "ssl_ca": "/workspaces/oop-assignment/python/static/DigiCertGlobalRootCA.crt.pem",
+    "ssl_disabled": False
 }
 
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(**config)
+        return conn
+    except Error as e:
+        print("Error connecting to MySQL database:", e)
+        return None
 
-try:
-    # Try to establish a connection
-    cnx = mysql.connector.connect(**config)
-    
-    # Check if the connection is successful
-    if cnx.is_connected():
-        print("Connection successful!")
-        
-        # Execute a simple query to test the connection
-        cursor = cnx.cursor()
-        cursor.execute("SELECT 1")
-        result = cursor.fetchone()
-        print("Query result:", result)
-        
-    else:
-        print("Connection failed.")
-        
-except mysql.connector.Error as e:
-    print("Error connecting to MySQL database:", e)
-    
-finally:
-    # Close the connection
-    cnx.close()
+def query_db(query, args=(), fetch_one=False):
+    conn = get_db_connection()
+    if not conn:
+        return None
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query, args)
+    rv = cursor.fetchone() if fetch_one else cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return rv
